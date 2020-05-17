1fd5e148f5b4c0e199c0edd9569031d538720afe
Dennis Kwabena Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2020-05-17 05:27:38 +0000

    Reformatted code

--- lib/Screens/MonitoringPage.dart
+++ lib/Screens/MonitoringPage.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 
+enum Confirmation { Cancel, Accept }
 
-
-enum Confirmation{Cancel,Accept}
-
-class MonitoringPage extends StatelessWidget{
-
+class MonitoringPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -13,244 +10,212 @@ class MonitoringPage extends StatelessWidget{
       title: 'Monitoring Page',
       theme: ThemeData(
         primarySwatch: Colors.cyan,
-
       ),
       home: MyMonitor(title: 'Monitor'),
     );
   }
 }
-class MyMonitor extends StatefulWidget{
+
+class MyMonitor extends StatefulWidget {
   int index;
 
   MyMonitor({Key key, this.title}) : super(key: key);
   final String title;
 
   @override
-  _MyMonitorState  createState()  => _MyMonitorState(index);
+  _MyMonitorState createState() => _MyMonitorState(index);
 }
 
-class _MyMonitorState extends State<MyMonitor>{
-
-  final List<String>listof=['Leaving Home','Entering Home','Going to Bed','Rise and Shine','Allow or Deny Entry'];
-
+class _MyMonitorState extends State<MyMonitor> {
+  final List<String> listof = [
+    'Leaving Home',
+    'Entering Home',
+    'Going to Bed',
+    'Rise and Shine',
+    'Allow or Deny Entry'
+  ];
 
   _MyMonitorState(int index);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title:Text(widget.title),
-            actions: <Widget>[
-              new IconButton(
-                icon: new Icon(Icons.search),
-                onPressed: ()=>debugPrint('Search'),
-              )
-            ]
-        ),
-
-
-        body: new Card(
-       child: ListView.builder(
-         itemCount:listof.length,
-            itemBuilder: (context,index) 
-             {
-    var post = listof[index];
-                return ListTile(
-                  title: Text(post),
-      onTap: (){
-        if(index==0){
-          Card(child: 
-          new CircleAvatar(
-             child: new Text(listof[0]
-             ),
-          backgroundColor: Colors.cyan,
-          foregroundColor:Colors.white ));
-Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) {
-              return AlertDialog(
-        title: Text('Leaving Home'),
-        content: const Text(
-            'This will reset your devices on alert and all doors closed.'),
-        actions: <Widget>[
-          FlatButton(
-            child: const Text('CANCEL'),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-          FlatButton(
-            child: const Text('ACCEPT'),
-            onPressed: () {
-              Navigator.push(context, _ackAlert(context));
-            },
-          )
-        ],
-      );
-            },
+      appBar: AppBar(title: Text(widget.title), actions: <Widget>[
+        new IconButton(
+          icon: new Icon(Icons.search),
+          onPressed: () => debugPrint('Search'),
+        )
+      ]),
+      body: new Card(
+          child: ListView.builder(
+        itemCount: listof.length,
+        itemBuilder: (context, index) {
+          var post = listof[index];
+          return ListTile(
+            title: Text(post),
+            onTap: () {
+              if (index == 0) {
+                Card(
+                    child: new CircleAvatar(
+                        child: new Text(listof[0]),
+                        backgroundColor: Colors.cyan,
+                        foregroundColor: Colors.white));
+                Navigator.push(context, MaterialPageRoute(
+                  builder: (context) {
+                    return AlertDialog(
+                      title: Text('Leaving Home'),
+                      content: const Text(
+                          'This will reset your devices on alert and all doors closed.'),
+                      actions: <Widget>[
+                        FlatButton(
+                          child: const Text('CANCEL'),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                        FlatButton(
+                          child: const Text('ACCEPT'),
+                          onPressed: () {
+                            Navigator.push(context, _ackAlert(context));
+                          },
                         )
-                      );
-                    }
-            
-               else if(index==1){
-                 Card(child: 
-                 new CircleAvatar(
-                         child: new Text(listof[1]
-                         ),
-                      backgroundColor: Colors.cyan,
-                      foregroundColor:Colors.white ));
-                Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) {
-                          return AlertDialog(
-        title: Text('Entering Home'),
-        content: const Text(
-            'This will reset your devices on alert and all doors closed.'),
-        actions: <Widget>[
-          FlatButton(
-            child: const Text('CANCEL'),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-          FlatButton(
-            child: const Text('ACCEPT'),
-            onPressed: () {
-              Navigator.push(context, _ackAlert4(context));
-            },
-          )
-        ],
-      );
-                        }
-                        )
-                      );
-            }
-            else if(index==2){
-              Card(child: 
-              new CircleAvatar(
-                         child: new Text(listof[2]
-                         ),
-                      backgroundColor: Colors.cyan,
-                      foregroundColor:Colors.white ));
-              Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context){
-                          return AlertDialog(
-        title: Text('Bed Time'),
-        content: const Text(
-            'This will reset your devices on alert and all doors closed.'),
-        actions: <Widget>[
-          FlatButton(
-            child: const Text('CANCEL'),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-          FlatButton(
-            child: const Text('ACCEPT'),
-            onPressed: () {
-              Navigator.push(context, _ackAlert3(context));
-            },
-          )
-        ],
-      );
-                        },
-                        )
-                      );
-            }
-            
-            else if(index==3){
-              Card(child: 
-              new CircleAvatar(
-                         child: new Text(listof[3]
-                         ),
-                      backgroundColor: Colors.cyan,
-                      foregroundColor:Colors.white ));
-              Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) { return AlertDialog(
-        title: Text('Awake'),
-        content: const Text(
-            'This will reset your devices on alert and all doors closed.'),
-        actions: <Widget>[
-          FlatButton(
-            child: const Text('CANCEL'),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-          FlatButton(
-            child: const Text('ACCEPT'),
-            onPressed: () {
-              Navigator.push(context, _ackAlert2(context));
-            },
-          )
-        ],
-      );
-                                         }                 )
-                        
-                      );
-            }
-            else{
-              Card(child:   new CircleAvatar(
-                         child: new Text(listof[4]
-                         ),
-                      backgroundColor: Colors.cyan,
-                      foregroundColor:Colors.white ));
-              Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context){                   
-      return AlertDialog(
-        title: Text('Allow Entry'),
-        content: const Text(
-            'This will allow doors to open automatically.'),
-        actions: <Widget>[
-          FlatButton(
-            child: const Text('CANCEL'),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-          FlatButton(
-            child: const Text('ACCEPT'),
-            onPressed: () {
-              Navigator.push(context, _ackAlert1(context));
-            },
-          )
-        ],
-      );
-    
-
-                        },
-                        )
-                      );
-            }
-            
+                      ],
+                    );
                   },
+                ));
+              } else if (index == 1) {
+                Card(
+                    child: new CircleAvatar(
+                        child: new Text(listof[1]),
+                        backgroundColor: Colors.cyan,
+                        foregroundColor: Colors.white));
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return AlertDialog(
+                    title: Text('Entering Home'),
+                    content: const Text(
+                        'This will reset your devices on alert and all doors closed.'),
+                    actions: <Widget>[
+                      FlatButton(
+                        child: const Text('CANCEL'),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                      FlatButton(
+                        child: const Text('ACCEPT'),
+                        onPressed: () {
+                          Navigator.push(context, _ackAlert4(context));
+                        },
+                      )
+                    ],
+                  );
+                }));
+              } else if (index == 2) {
+                Card(
+                    child: new CircleAvatar(
+                        child: new Text(listof[2]),
+                        backgroundColor: Colors.cyan,
+                        foregroundColor: Colors.white));
+                Navigator.push(context, MaterialPageRoute(
+                  builder: (context) {
+                    return AlertDialog(
+                      title: Text('Bed Time'),
+                      content: const Text(
+                          'This will reset your devices on alert and all doors closed.'),
+                      actions: <Widget>[
+                        FlatButton(
+                          child: const Text('CANCEL'),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                        FlatButton(
+                          child: const Text('ACCEPT'),
+                          onPressed: () {
+                            Navigator.push(context, _ackAlert3(context));
+                          },
+                        )
+                      ],
+                    );
+                  },
+                ));
+              } else if (index == 3) {
+                Card(
+                  child: new CircleAvatar(
+                      child: new Text(listof[3]),
+                      backgroundColor: Colors.cyan,
+                      foregroundColor: Colors.white),
                 );
-              },
-                        // =>listDataItem(this.listof[index]),)
-            
-                     )),
-                  floatingActionButton: new FloatingActionButton(onPressed: ()=>debugPrint('FB Button Click'),
-                    child: new Icon(Icons.live_help),
-                   backgroundColor: Colors.cyan,
-                   foregroundColor: Colors.white),
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Text('Awake'),
+                        content: const Text(
+                            'This will reset your devices on alert and all doors closed.'),
+                        actions: <Widget>[
+                          FlatButton(
+                            child: const Text('CANCEL'),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                          FlatButton(
+                            child: const Text('ACCEPT'),
+                            onPressed: () {
+                              Navigator.push(context, _ackAlert2(context));
+                            },
+                          )
+                        ],
+                      );
+                    },
+                  ),
                 );
-            
+              } else {
+                Card(
+                    child: new CircleAvatar(
+                        child: new Text(listof[4]),
+                        backgroundColor: Colors.cyan,
+                        foregroundColor: Colors.white));
+                Navigator.push(context, MaterialPageRoute(
+                  builder: (context) {
+                    return AlertDialog(
+                      title: Text('Allow Entry'),
+                      content: const Text(
+                          'This will allow doors to open automatically.'),
+                      actions: <Widget>[
+                        FlatButton(
+                          child: const Text('CANCEL'),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                        FlatButton(
+                          child: const Text('ACCEPT'),
+                          onPressed: () {
+                            Navigator.push(context, _ackAlert1(context));
+                          },
+                        )
+                      ],
+                    );
+                  },
+                ));
               }
-            
+            },
+          );
+        },
+        // =>listDataItem(this.listof[index]),)
+      )),
+      floatingActionButton: new FloatingActionButton(
+          onPressed: () => debugPrint('FB Button Click'),
+          child: new Icon(Icons.live_help),
+          backgroundColor: Colors.cyan,
+          foregroundColor: Colors.white),
+    );
+  }
+}
 
-            
-            }
-            
-           
 _ackAlert1(BuildContext context) {
   return showDialog<void>(
     context: context,
@@ -270,6 +235,7 @@ _ackAlert1(BuildContext context) {
     },
   );
 }
+
 _ackAlert2(BuildContext context) {
   return showDialog<void>(
     context: context,
@@ -289,6 +255,7 @@ _ackAlert2(BuildContext context) {
     },
   );
 }
+
 _ackAlert3(BuildContext context) {
   return showDialog<void>(
     context: context,
@@ -328,6 +295,7 @@ _ackAlert4(BuildContext context) {
     },
   );
 }
+
 _ackAlert(BuildContext context) {
   return showDialog<void>(
     context: context,
